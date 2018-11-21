6312bf54a4a3f0ca24b3770c39d6b03801ffbc80
yn2951 <44895656+yn2951@users.noreply.github.com>
2018-11-21 21:06:16 +0900

    Add files via upload

--- user.rb
+++ user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   validates :name, length: { in: 1..15 }
-  validates :email, presence: true
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, length: { in: 8..32 }
-  validates :password, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates :email, presence: true , format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,32}+\z/ }
 
   has_secure_password
+
+  has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
 end
