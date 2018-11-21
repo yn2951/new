7a980337764813f7aee6222348faaf790577ff2a
yn2951 <44895656+yn2951@users.noreply.github.com>
2018-11-21 20:23:27 +0900

    Add files via upload

---
+++ じゃんけん.rb
@@ -0,0 +1,54 @@
+puts "じゃんけん・・・・"
+puts "[0]グー\n[1]チョキ\n[2]パー"
+
+def game
+  you = gets.to_i
+  cpu = rand(0..2)
+
+  if you == 0
+    puts "YOU: グー"
+    if cpu == 0
+      puts "CPU: グー"
+      puts "あいこで"
+      game
+    elsif cpu == 1
+      puts "CPU: チョキ"
+      puts "勝ち"
+    else
+      puts "CPU: パー"
+      puts "負け"
+    end
+  end
+
+  if you == 1
+    puts "YOU: チョキ"
+    if cpu == 1
+      puts "CPU: チョキ"
+      puts "あいこで"
+      game
+    elsif cpu == 2
+      puts "CPU: パー"
+      puts "勝ち"
+    else
+      puts "CPU: グー"
+      puts "負け"
+    end
+  end
+
+  if you == 2
+    puts "YOU: パー"
+    if cpu == 2
+      puts "CPU: パー"
+      puts "あいこで"
+      game
+    elsif cpu == 0
+      puts "CPU: グー"
+      puts "勝ち"
+    else
+      puts "CPU: チョキ"
+      puts "負け"
+    end
+  end
+end
+
+game
